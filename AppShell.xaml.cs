c7b1fb2ca833756658f6a0bac147f5948afdf2fc
Rhys Tedeschi <rtedeschi@3Tec.de>
2024-07-26 09:30:25 +0200

    First commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Timetracker
+{
+  public partial class AppShell : Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Timetracker
+{
+  [Register("AppDelegate")]
+  public class AppDelegate : MauiUIApplicationDelegate
+  {
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+  }
+}
